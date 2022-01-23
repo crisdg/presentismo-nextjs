fee9ebaa44a3b071d88413a2923cb70fa740983d
Cristian <cristiangomez.mail@gmail.com>
2022-01-23 19:36:45 -0300

    se soluciona bug que no guardaba primer registro en dayStatusForm

--- src/dayStatusForm.js
+++ src/dayStatusForm.js
@@ -7,6 +7,13 @@ export default function DayStatusForm() {
   const [date, setDate] = useState("")
   const [regId, setRegId] = useState()
 
+  const [puesto, setPuesto] = useState()
+  const [status, setStatus] = useState()
+  const [nombre, setNombre] = useState()
+  const [apellido, setApellido] = useState()
+  const [fecha, setFecha] = useState()
+  const [idEmpleado, setIdEmpleado] = useState()
+
   useEffect(() => {
     fetch("/api/getEmployees")
       .then((res) => res.json())
@@ -19,8 +26,6 @@ export default function DayStatusForm() {
   const saveStatus = async (e) => {
     e.preventDefault()
 
-    console.log("save")
-
     try {
       const res = await fetch("/api/absenteeism/createDayStatus", {
         method: "POST",
@@ -32,13 +37,13 @@ export default function DayStatusForm() {
           status: e.target.status.value,
           nombre: e.target.nombre.value,
           apellido: e.target.apellido.value,
-
-          fecha: e.target.fecha.value,
+          fecha: date,
           id_empleado: e.target.id_empleado.value,
         }),
       })
-
+      console.log(res)
       const json = await res.json()
+      setIndexControl(indexControl + 1)
 
       if (!res.ok) throw Error(json.message)
     } catch (e) {
@@ -50,49 +55,45 @@ export default function DayStatusForm() {
     <>
       {employeesData
         .slice(indexControl - 1, indexControl)
-        .map((employee, index) => (
-          <>
-            <form onSubmit={saveStatus}>
-              <input
-                type='date'
-                onChange={(e) => {
-                  setDate(e.target.value)
-                }}
-                value={date}
-                required
-                id='fecha'
-              />
-              <input
-                type='text'
-                value={employee.id}
-                readOnly
-                required
-                id='id_empleado'
-              />
-              <input type='text' value={employee.apeliido} id='apellido' />
-              <input type='text' value={employee.nombre} id='nombre' />
-              <select name='puesto' id='puesto'>
-                <option value='picking'>Picking</option>
-                <option value='periferia'>Periferia</option>
-                <option value='externo'>Externo</option>
-              </select>
-              <select name='status' id='status'>
-                <option value='presente'>Presente</option>
-                <option value='ausente con aviso'>Aus. con aviso</option>
-                <option value='ausente sin aviso'>Aus. sin aviso</option>
-                <option value='tarde'>Tarde</option>
-                <option value='retiro'>Retiro</option>
-              </select>
-              <button
-                onClick={() => {
-                  setIndexControl(indexControl + 1)
-                }}
-              >
-                Next
-              </button>
-            </form>
-          </>
-        ))}
+        .map((employee, index) => {
+          return (
+            <>
+              <form onSubmit={saveStatus}>
+                <input
+                  type='date'
+                  onChange={(e) => {
+                    setDate(e.target.value)
+                  }}
+                  value={date}
+                  required
+                  id='fecha'
+                />
+                <input
+                  type='text'
+                  value={employee.id}
+                  readOnly
+                  required
+                  id='id_empleado'
+                />
+                <input type='text' value={employee.apeliido} id='apellido' />
+                <input type='text' value={employee.nombre} id='nombre' />
+                <select name='puesto' id='puesto' defaultValue='picking'>
+                  <option value='picking'>Picking</option>
+                  <option value='periferia'>Periferia</option>
+                  <option value='externo'>Externo</option>
+                </select>
+                <select name='status' id='status' defaultValue='Presente'>
+                  <option value='presente'>Presente</option>
+                  <option value='ausente con aviso'>Aus. con aviso</option>
+                  <option value='ausente sin aviso'>Aus. sin aviso</option>
+                  <option value='tarde'>Tarde</option>
+                  <option value='retiro'>Retiro</option>
+                </select>
+                <button>Next</button>
+              </form>
+            </>
+          )
+        })}
     </>
   )
 }
